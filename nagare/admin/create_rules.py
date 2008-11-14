24f7c7839b3aef44ac9bd32afbc6e3b4c35f8135
Alain Poirier <alain.poirier@net-ng.com>
2008-11-14 17:45:46 +0000

    Apache rewrite rules generation added (default generation)
    --HG--
    extra : convert_revision : svn%3Afc25bd86-f976-46a1-be41-59ef0291ea8c/trunk%4029

--- nagare/admin/create_rules.py
+++ nagare/admin/create_rules.py
@@ -9,107 +9,136 @@
 
 """The ``create-rules`` administrative command
 
-Generate the rewrite rules for the `lighttpd <http://www.lighttpd.net/>`_ and
-`nginx <http://nginx.net/>`_ http server, to let them serve the static contents
-of the launched applications instead of the applications themself.
+Generate the rewrite rules for the `apache <http://httpd.apache.org/>`_,
+`lighttpd <http://www.lighttpd.net/>`_ or `nginx <http://nginx.net/>`_ http
+servers, to let them serve the static contents of the launched applications
+instead of the applications themselves.
 """
 
-import os, pkg_resources
+import os, pkg_resources, itertools
 from nagare.admin import util
 
 def set_options(optparser):
     """Register the possible options
-    
+
     They are:
-    
-      - ``-l`` or ``--lighttpd`` to generate the lighttpd rules (default)
+
+      - ``-a`` or ``--apache`` to generate the apache rules (default)
+      - ``-l`` or ``--lighttpd`` to generate the lighttpd rules
       - ``-n`` or ``--nginx`` to generate the nginx rules
-      
+
     In:
       - ``optparser`` -- the options parser
     """
     optparser.usage += ' [applications ...]'
 
-    optparser.add_option('-l', '--lighttpd', action='store_const', const='l', dest='server', help='generate lighttpd rules', default='l')
-    optparser.add_option('-n', '--nginx', action='store_const', const='n', dest='server', help='generate nginx rules')
+    optparser.add_option('-a', '--apache', action='store_const', const=generate_apache_rules, dest='generate', help='generate apache rules', default=generate_apache_rules)
+    optparser.add_option('-l', '--lighttpd', action='store_const', const=generate_lighttpd_rules, dest='generate', help='generate lighttpd rules')
+    optparser.add_option('-n', '--nginx', action='store_const', const=generate_nginx_rules, dest='generate', help='generate nginx rules')
 
 def create_rules(app_names, error):
     """Generate the rewrite rules for the given registered applications
-    
+
     In:
       - ``app_names`` -- list of the applications, read from the command line
       - ``error`` -- the function to call in case of configuration errors
-      
+
     Return:
       - list of tuples (application, directory of its static contents)
-    """    
+    """
     # If no applications is given on the command line, generate the rules
     # for all the registered applications
     if not app_names:
         app_names = [entry.name for entry in pkg_resources.iter_entry_points('nagare.applications')]
-        
+
     package = pkg_resources.Requirement.parse('nagare')
     static = pkg_resources.resource_filename(package, 'static')
-    
+
     apps = [('nagare', static)]   # Initialise the result tuple with the static contents of the framework
-    
+
     for app_name in app_names:
         (cfgfile, app, dist, aconf) = util.read_application(app_name, error)
-    
+
         static = aconf['application'].get('static', os.path.join(dist.location, 'static') if dist else None)
-    
+
         if static and not os.path.isdir(static):
             static = None
-    
+
         apps.append((app_name, static))
-    
+
     return sorted(apps, key=lambda x: len(x[0]))
+
+def commonprefix(filenames):
+    filenames = [filename.split(os.sep) for filename in filenames]
+
+    l = [map(t[0].__eq__, t) for t in zip(*filenames)]
+    n = len(list(itertools.takewhile(lambda x: all(x), l)))
+    
+    return os.sep.join(filenames[0][:n]+[''])
 
 def generate_lighttpd_rules(app_names, error):
     """Generate the lighttpd rewrite rules for the given registered applications
-    
+
     In:
       - ``app_names`` -- list of the applications, read from the command line
       - ``error`` -- the function to call in case of configuration errors
-      
+
     Return:
       - yield the lighttpd configuration file fragment
-    """    
+    """
     apps = create_rules(app_names, error)
-    document_root = os.path.commonprefix([static for (name, static) in apps])
-    
+    document_root = commonprefix([static for (name, static) in apps])
+
     yield 'server.document-root = "%s"' % document_root
     yield ''
-    
+
     yield 'url.rewrite = ('
     for (app_name, static) in apps:
         yield '  "^/static/%s/(.*)" => "%s/$1",' % (app_name, static[len(document_root):])
-    
+
     yield '  "^(.*)" => "/fcgi/$1"'
     yield ')'
 
 def generate_nginx_rules(app_names, error):
     """Generate the nginx rewrite rules for the given registered applications
-    
+
     In:
       - ``app_names`` -- list of the applications, read from the command line
       - ``error`` -- the function to call in case of configuration errors
-      
+
     Return:
       - yield the nginx configuration file fragment
-    """    
+    """
     for (app_name, static) in create_rules(app_names, error):
         yield 'location /static/%s/ {' % app_name
         yield '  alias %s/;' % static
         yield '}'
         yield ''
 
-def run(parser, options, args):
-    if options.server == 'l':
-        print '\n'.join(generate_lighttpd_rules(args, parser.error))
+def generate_apache_rules(app_names, error):
+    """Generate the apache rewrite rules for the given registered applications
+
+    In:
+      - ``app_names`` -- list of the applications, read from the command line
+      - ``error`` -- the function to call in case of configuration errors
+
+    Return:
+      - yield the apache configuration file fragment
+    """
+    apps = create_rules(app_names, error)
+    document_root = commonprefix([static for (name, static) in apps])
     
-    if options.server == 'n':
-        print '\n'.join(generate_nginx_rules(args, parser.error))
+    yield 'DocumentRoot %s' % document_root
+    yield ''
+
+    yield 'RewriteEngine On'
+    for (app_name, static) in apps:
+        yield 'RewriteRule ^/static/%s/(.*)$ /%s/$1 [L]' % (app_name, static[len(document_root):])
+        
+    yield 'RewriteRule ^/(.*)$ /fcgi/$1 [QSA,L]'
+
+def run(parser, options, args):
+    print '\n'.join(options.generate(args, parser.error))
 
 # ---------------------------------------------------------------------------
 
