d543be18d6d08b0edbdaddc70420ba8cae3b9746
Alain Poirier <alain.poirier@net-ng.com>
2009-05-11 16:27:11 +0000

    No validation at the creation
    --HG--
    extra : convert_revision : svn%3Afc25bd86-f976-46a1-be41-59ef0291ea8c/trunk%40120

--- nagare/test/test_form.py
+++ nagare/test/test_form.py
@@ -12,14 +12,14 @@ from nagare.namespaces import xhtml
 
 class MyEditor(editor.Editor):
     fields = ('name', 'age')
-    
+
     def __init__(self, source):
         super(MyEditor, self).__init__(source, self.fields)
 
         self.name.validate(lambda v: validator.to_string(v, strip=True).not_empty().to_string())
         self.age.validate(lambda v: validator.to_int(v).lesser_than(50).greater_than(0).to_int())
-        
-        
+
+
     def commit(self, comp):
         if super(MyEditor, self).commit(self.fields):
             comp.answer((self.name()))
@@ -36,11 +36,11 @@ class MyApp:
         self.name = 'Foo'
         self.age  = 5
 
-        
+
 def call(o1, o2, model=None):
     component.call_wrapper(lambda: o1.call(o2, model))
-    
-        
+
+
 def test1():
     """ Form - validation OK """
     o = MyApp()
@@ -48,7 +48,7 @@ def test1():
     app = component.Component(o, model=None)
     assert app().name == 'Foo'
     assert app().age  == 5
-    
+
     editor = MyEditor(app())
 
     call(app, editor)
@@ -80,7 +80,7 @@ def test2():
     # App values doesn't changes
     assert o.name == 'Foo'
     assert o.age  == 5
-    
+
     # Validation OK
     editor.setValues('Bar', 10, app)
     assert o.name == 'Bar'
@@ -95,7 +95,7 @@ def test3():
 
     editor = MyEditor(app())
     call(app, editor)
-    
+
     # Validation KO
     editor.setValues('Bar', 1000, app)
     # Editor values changes
@@ -104,21 +104,21 @@ def test3():
     # App values doesn't changes
     assert o.name == 'Foo'
     assert o.age  == 5
-    
+
     # Cancel
     app.answer()
     assert o.name == 'Foo'
     assert o.age  == 5
-    
+
 
 
 class MyStringEditor(editor.Editor):
     fields = ('name',)
-    
+
     def __init__(self, source):
         super(MyStringEditor, self).__init__(source, self.fields)
-        
-        
+
+
     def commit(self, comp):
         if super(MyStringEditor, self).commit(self.fields):
             comp.answer((self.name()))
@@ -127,7 +127,7 @@ class MyStringEditor(editor.Editor):
     def setValues(self, name, comp):
         self.name(name)
         self.commit(comp)
-    
+
 
 class MyStringEditor1(MyStringEditor):
 
@@ -135,8 +135,8 @@ class MyStringEditor1(MyStringEditor):
         super(MyStringEditor1, self).__init__(source)
         self.name.validate(lambda v: validator.to_string(v, strip=True).not_empty())
 
-    
-    
+
+
 def test4():
     """ Form - test string validators - not_empty """
     o = MyApp()
@@ -145,23 +145,23 @@ def test4():
 
     editor = MyStringEditor1(app())
     call(app, editor)
-    
+
     # Validation KO
     editor.setValues('', app)
     assert editor.name.error == "Can't be empty"
 
     editor.setValues('abcd', app)
     assert editor.name.error is None
-    
-    
-    
+
+
+
 class MyStringEditor2(MyStringEditor):
 
     def __init__(self, source):
         super(MyStringEditor2, self).__init__(source)
         self.name.validate(lambda v: validator.to_string(v, strip=True).match(r'^[a-d]+$', msg="test5 - Incorrect format"))
-   
-   
+
+
 def test5():
     """ Form - test string validators - match """
     o = MyApp()
@@ -170,11 +170,11 @@ def test5():
 
     editor = MyStringEditor2(app())
     call(app, editor)
-    
+
     # Validation KO
     editor.setValues('abrab', app)
     assert editor.name.error == "test5 - Incorrect format"
-    
+
     editor.setValues('abab', app)
     assert editor.name.error is None
 
@@ -184,8 +184,8 @@ class MyStringEditor3(MyStringEditor):
     def __init__(self, source):
         super(MyStringEditor3, self).__init__(source)
         self.name.validate(lambda v: validator.to_string(v, strip=True).shorter_than(5, msg="test6 - Length must be shorter than %(max)d characters"))
-   
-   
+
+
 def test6():
     """ Form - test string validators - shorter_than """
     o = MyApp()
@@ -194,11 +194,11 @@ def test6():
 
     editor = MyStringEditor3(app())
     call(app, editor)
-    
+
     # Validation KO
     editor.setValues('123456', app)
     assert editor.name.error == "test6 - Length must be shorter than 5 characters"
-    
+
     editor.setValues('1234', app)
     assert editor.name.error is None
 
@@ -208,8 +208,8 @@ class MyStringEditor4(MyStringEditor):
     def __init__(self, source):
         super(MyStringEditor4, self).__init__(source)
         self.name.validate(lambda v: validator.to_string(v, strip=True).length_equal(5, msg="test7 - Length must be %(len)d characters"))
-   
-   
+
+
 def test7():
     """ Form - test string validators - length_equal  """
     o = MyApp()
@@ -218,11 +218,11 @@ def test7():
 
     editor = MyStringEditor4(app())
     call(app, editor)
-    
+
     # Validation KO
     editor.setValues('123456', app)
     assert editor.name.error == "test7 - Length must be 5 characters"
-    
+
     editor.setValues('1234', app)
     assert editor.name.error == "test7 - Length must be 5 characters"
 
@@ -235,8 +235,8 @@ class MyStringEditor5(MyStringEditor):
     def __init__(self, source):
         super(MyStringEditor5, self).__init__(source)
         self.name.validate(lambda v: validator.to_string(v, strip=True).longer_than(5).shorter_or_equal_than(8).not_empty().match(r'^[1-9]+$'))
-   
-   
+
+
 def test8():
     """ Form - test string validators - multiple validators """
     o = MyApp()
@@ -245,11 +245,11 @@ def test8():
 
     editor = MyStringEditor5(app())
     call(app, editor)
-    
+
     # Validation KO
     editor.setValues('', app)
     assert editor.name.error is not None
-    
+
     editor.setValues('1234', app)
     assert editor.name.error is not None
 
@@ -258,7 +258,7 @@ def test8():
 
     editor.setValues('abcdefg', app)
     assert editor.name.error is not None
-    
+
     editor.setValues('123456', app)
     assert editor.name.error is None
 
@@ -281,7 +281,7 @@ def test11():
 
 def test12():
     p = editor.Property(15).validate(check)
-    assert (p.input == 15) and (p.value is None) and (p.error == 'invalid')
+    assert (p.input == 15) and (p.value == 15) and (p.error is None)
 
 def test13():
     p = editor.Property().validate(check)
