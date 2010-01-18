658f790670382933a4edfce82dfef3c9db6b777e
Alain Poirier <alain.poirier@net-ng.com>
2010-01-18 11:37:21 +0000

    Fixes #47
    --HG--
    extra : convert_revision : svn%3Afc25bd86-f976-46a1-be41-59ef0291ea8c/trunk%40191

--- nagare/admin/util.py
+++ nagare/admin/util.py
@@ -34,11 +34,11 @@ class Command:
 
 def load_entry_point(app, entry_point):
     """Load an object registered under an entry point
-    
+
     In:
       - ``app`` -- name of the object
       - ``entry_point`` -- name of the entry_point
-      
+
     Return:
       - (the object, the distribution of the object)
     """
@@ -50,26 +50,26 @@ def load_entry_point(app, entry_point):
 
 def load_app(app, _):
     """Load a registered application
-    
+
     In:
       - ``app`` -- name of the application
       - ``_`` -- *unused**
-      
+
     Return:
-      - (the application, the distribution of the application) 
+      - (the application, the distribution of the application)
     """
     return load_entry_point(app, 'nagare.applications')
 
 
 def load_egg(dist, app):
     """Load a registered application of a distribution
-    
+
     In:
       - ``dist`` -- name of the distribution
       - ``app`` -- name of the application
-      
+
     Return:
-      - (the application, the distribution of the application) 
+      - (the application, the distribution of the application)
     """
     dist = pkg_resources.get_distribution(dist)
     return (dist.get_entry_info('nagare.applications', app).load(), dist)
@@ -77,11 +77,11 @@ def load_egg(dist, app):
 
 def load_file(filename, app):
     """Load an object from a file
-    
+
     In:
       - ``filename`` -- name of the file
       - ``app`` -- name of the object to load
-      
+
     Return:
       - (the object, None)
     """
@@ -95,11 +95,11 @@ def load_file(filename, app):
 
 def load_module(module, app):
     """Load an object from a Python module
-    
+
     In:
       - ``module`` -- name of the module
       - ``app`` -- name of the object to load
-      
+
     Return:
       - (the object, None)
     """
@@ -107,8 +107,8 @@ def load_module(module, app):
 
     if app is not None:
         r = getattr(r, app)
-    
-    return (r, None)    
+
+    return (r, None)
 
 loaders = {
             '' : load_module,
@@ -120,20 +120,20 @@ loaders = {
 
 def load_object(path):
     """Load an object from a path
-    
+
     The possible path syntaxes are:
-    
+
       - ``'python <module>:<object>'`` -- loads an object from a Python module
         (for example: ``'python os.path:isfile'``)
       - ``'<module>:<object>'``  -- same as ``'python <module>:<object>'``
       - ``'file <file>:<object>'`` -- loads an object from a file
-        (for example: ``'file /tmp/counter.py:Counter'``) 
+        (for example: ``'file /tmp/counter.py:Counter'``)
       - ``'egg <dist>:<app>'`` -- loads the registered application ``<app>``
         from the ``<dist>`` distribution
         (for example: ``'egg nagare:admin'`` or ``'egg nagare.examples:wiki'``)
       - ``'app <app>'`` -- load the registered application ``<app>``
         (for example: ``'app examples'``)
-      
+
     Return:
       - a tuple (object loaded, distribution where this object is located or ``None``)
     """
@@ -141,12 +141,12 @@ def load_object(path):
         scheme, path = path.split(' ', 1)
     else:
         scheme = ''
-        
+
     if ':' in path:
         (path, o) = path.split(':', 1)
     else:
         o = None
-        
+
     return loaders[scheme](path, o)
 
 # ---------------------------------------------------------------------------
@@ -159,7 +159,7 @@ application_options_spec = {
         path = 'string()', # Where to find the application object (see ``load_object()``) ?
         name = 'string()', # URL for the application
         debug = 'boolean(default=False)', # Debug web page activated ?
-                        
+
         redirect_after_post = 'boolean(default=False)', # Follow the PRG pattern ?
         always_html = 'boolean(default=True)', # Don't generate xhtml, even if it's a browser capability ?
         wsgi_pipe = 'string(default="")' # Method to create the WSGI middlewares pipe
@@ -176,27 +176,27 @@ application_options_spec = {
         )
     )
 }
-    
+
 def read_application_options(cfgfile, error, default={}):
     """Read the configuration file for the application
-    
+
     In:
       - ``cfgfile`` -- path to an application configuration file
       - ``error`` -- the function to call in case of configuration errors
-      - ``default`` -- optional default values      
-      
+      - ``default`` -- optional default values
+
     Return:
-      - a ``ConfigObj`` of the application parameters    
-    """    
+      - a ``ConfigObj`` of the application parameters
+    """
     spec = configobj.ConfigObj(default)
     spec.merge(application_options_spec)
 
     choices = ', '. join(['"%s"' % entry.name for entry in pkg_resources.iter_entry_points('nagare.sessions')])
     spec.merge({ 'sessions' : { 'type' : 'option(%s, default="")' % (choices + ', ""') } })
 
-    conf = configobj.ConfigObj(cfgfile, configspec=spec, interpolation='Template' if default else None)    
+    conf = configobj.ConfigObj(cfgfile, configspec=spec, interpolation='Template' if default else None)
     config.validate(cfgfile, conf, error)
-    
+
     # The database sub-sections inherit from the database section
     spec['database']['__many__'].merge(dict(
                             uri = 'string(default=%s)' % str(conf['database']['uri']),
@@ -208,20 +208,20 @@ def read_application_options(cfgfile, error, default={}):
 
     if not conf['sessions']['type']:
         del conf['sessions']['type']
-    
+
     return conf
 
 
 def read_application(cfgfile, error):
     """Read the configuration file for the application and create the application object
-    
+
     In:
       - ``cfgfile`` -- name of a registered application or path to an application configuration file
       - ``error`` -- the function to call in case of configuration errors
-      
+
     Return:
       - a tuple:
-      
+
         - name of the application configuration file
         - the application object
         - the distribution of the application
@@ -229,45 +229,45 @@ def read_application(cfgfile, error):
     """
     if not os.path.isfile(cfgfile):
         # The name of a registered application is given, find its configuration file
-        
+
         # Get all the registered applications
         apps = dict([(entry.name, entry) for entry in pkg_resources.iter_entry_points('nagare.applications')])
-    
+
         # Get the application
         app = apps.get(cfgfile)
         if app is None:
             error("application '%s' not found (use 'nagare-admin serve' to see the available applications)" % cfgfile)
-        
+
         # From the directory of the application, get its configuration file
         requirement = pkg_resources.Requirement.parse(app.dist.project_name)
         cfgfile = pkg_resources.resource_filename(requirement, os.path.join('conf', cfgfile+'.cfg'))
-    
+
     # Read the application configuration file
     aconf = read_application_options(cfgfile, error)
 
     # From the path of the application, create the application object
     (app, dist) = load_object(aconf['application']['path'])
-    
+
     defaults = dict(here='string(default="%s")' % os.path.abspath(os.path.dirname(cfgfile)))
     if dist is not None:
         defaults['root'] = 'string(default="%s")' % dist.location
 
     # Re-read the application configuration, with some substitution variables
     aconf = read_application_options(cfgfile, error, defaults)
-    
+
     return (cfgfile, app, dist, aconf)
 
 # ---------------------------------------------------------------------------
 
 def set_metadata(conf, debug):
     """Activate the database metadata object
-    
+
     The location of the metadata object is read from the configuration file
-    
+
     In:
       - ``conf`` -- the ``ConfigObj`` object, created from the configuration file
       - ``debug`` -- debug mode for the database engine
-      
+
     Return:
       - the metadata object
     """
@@ -275,18 +275,18 @@ def set_metadata(conf, debug):
 
     if conf['activated'] and metadata:
         metadata = load_object(metadata)[0]
-        
+
         # All the parameters, of the [database] section, with an unknown name are
         # given to the database engine
         engine_conf = dict([(k, v) for (k, v) in conf.items() if k not in ('uri', 'activated', 'metadata', 'debug', 'populate')])
         database.set_metadata(metadata, conf['uri'], debug, **engine_conf)
-    
+
         return metadata
 
 
 def activate_WSGIApp(app, cfgfile, aconf, error, static_path=None, static_url=None, publisher=None, sessions_factory=None, debug=False):
     """
-    
+
     In:
       - ``app`` -- the application root object
       - ``cfgfile`` -- the path to the configuration file
@@ -298,7 +298,7 @@ def activate_WSGIApp(app, cfgfile, aconf, error, static_path=None, static_url=No
       - ``publisher`` -- the publisher of the application
       - ``session_factory`` -- the sessions managare factory
       - ``debug`` -- flag to display the generated SQL statements
-      
+
     Return:
       - a tuple:
           - the ``wsgi.WSGIApp`` object
@@ -323,10 +323,21 @@ def activate_WSGIApp(app, cfgfile, aconf, error, static_path=None, static_url=No
     app = wsgi.create_WSGIApp(app)
 
     app.set_config(cfgfile, aconf, error)
-    app.set_static_path(static_path)
-    app.set_static_url(static_url)
-    app.set_publisher(publisher)
-    app.set_sessions_factory(sessions_factory)
-    app.set_metadatas(metadatas)
+
+    if static_path is not None:
+        app.set_static_path(static_path)
+
+    if static_url is not None:
+        app.set_static_url(static_url)
+
+    if publisher:
+        app.set_publisher(publisher)
+
+    if sessions_factory:
+        app.set_sessions_factory(sessions_factory)
+
+    if metadatas:
+        app.set_metadatas(metadatas)
 
     return (app, zip(metadatas, populates))
+
