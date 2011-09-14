f62bc41b5e164249e663221c2a402e5daec5349f
Alain Poirier <alain.poirier@net-ng.com>
2011-09-14 19:38:51 +0200

    Mercurial references added

--- setup.py
+++ setup.py
@@ -50,9 +50,9 @@ setup(
 
       For a standard installation, read the ``docs/quickstart.txt`` document.
 
-      Read ``docs/framework_installation.txt`` to install the latest development
-      version from the
-      `Nagare subversion repository <svn://www.nagare.org/trunk/nagare/core#egg=nagare-dev>`_
+      Read ``docs/framework_installation.txt`` to install the
+      `latest <http://www.nagare.org/download/nagare-latest#egg=nagare-dev>`_
+      development version from the `Mercurial repository <http://hg.nagare.org/core>`_
       or to create a Nagare developer installation.
       """) % long_description,
       license = 'BSD',
