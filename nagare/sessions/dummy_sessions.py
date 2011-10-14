ecdefff873712f24b6936c2f42d446976f616d76
Alain Poirier <alain.poirier@net-ng.com>
2011-10-14 12:31:36 +0200

    Dummy sessions manager added. For completly stateless application like Restful web services

---
+++ nagare/sessions/dummy_sessions.py
@@ -0,0 +1,25 @@
+#--
+# Copyright (c) 2008-2011 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+#--
+
+"""Dummy sessions manager
+
+A new session is always created
+"""
+
+class State(object):
+    is_new = True
+    back_used = False
+    release = sessionid_in_url = sessionid_in_form = set = delete = lambda *args, **kw: ()
+
+class Sessions(object):
+    def set_config(self, filename, conf, error):
+        pass
+
+    def get(self, request, response, use_same_state):
+        return State()
--- setup.py
+++ setup.py
@@ -113,6 +113,7 @@ setup(
       standalone = nagare.sessions.memory_sessions:SessionsWithPickledStates
       pickle = nagare.sessions.memory_sessions:SessionsWithPickledStates
       memcache = nagare.sessions.memcached_sessions:Sessions
+      dummy = nagare.sessions.dummy_sessions:Sessions
 
       [nagare.applications]
       admin = nagare.admin.admin_app:app
