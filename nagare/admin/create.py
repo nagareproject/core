f780943485d7068c061ac99e397a6ad9a58e753f
Alain Poirier <alain.poirier@net-ng.com>
2011-11-28 18:26:00 +0100

    `create-app` admin command can now upgrade a project to 0.4.0

--- nagare/admin/create.py
+++ nagare/admin/create.py
@@ -16,71 +16,75 @@ from __future__ import with_statement
 
 import sys, os
 import textwrap
+import re
+from StringIO import StringIO
 
+from configobj import ConfigObj
 from nagare.admin import util
 
 def set_options(optparser):
     optparser.usage += ' application'
 
+def create_empty_file(filename):
+    open(filename, 'w').close()
 
-def create_empty_file(path, name):
-    open(os.path.join(path, name), 'w').close()
+def create_or_update(path, create=None, update=None, *args, **kw):
+    if not create:
+        create = lambda *args, **kw: None
+        
+    if not update:
+        update = lambda *args, **kw: None
+        
+    return (update if os.path.exists(path) else create)(path, *args, **kw)
 
+def create_setup_py(filename, params):
+    upgrade_setup_py(
+                     filename,
+                     params,
+                     textwrap.dedent('''\
+                        VERSION = '0.0.1'
+            
+                        from setuptools import setup, find_packages
+            
+                        setup(
+                              name = '%(name)s',
+                              version = VERSION,
+                              author = '',
+                              author_email = '',
+                              description = '',
+                              license = '',
+                              keywords = '',
+                              url = '',
+                              packages = find_packages(),
+                              include_package_data = True,
+                              package_data = {'' : ['*.cfg']},
+                              zip_safe = False,
+                              install_requires = ('nagare',),
+                              entry_points = """
+                              [nagare.applications]
+                              %(id)s = %(id)s.app:app
+                              """
+                             )
+                     ''' % params)
+                    )
 
-def run(parser, options, args):
-    if len(args) == 0:
-        parser.error('an application name must be provided')
+def upgrade_setup_py(filename, params, setup_py=None):
+    if not setup_py:
+        with open(filename, 'r') as f:
+            setup_py = f.read()
+            if 'message_extractors' in setup_py:
+                return
 
-    if len(args) > 1:
-        parser.error('only one application name must be provided')
+    if os.path.exists(filename):
+        os.rename(filename, filename+'.old')
+        
+    with open(filename, 'w') as f:
+        print >>f, re.sub(r'^(\s*)(entry_points)', r"\1message_extractors = { '%(id)s' : [('**.py', 'python', None)] },\n\1\2" % params, setup_py, flags=re.M)
 
-    root = args[0]
-    app_name = os.path.basename(root)
-    app_id = app_name.replace('-', '_')
+def upgrade_setup_cfg(filename):
+    old_conf = ConfigObj(filename)
 
-    if os.path.exists(root):
-        parser.error("the directory '%s' already exists" % root)
-    os.mkdir(root)
-
-    params = {
-        'exe' : sys.executable,
-        'root' : root,
-        'setup' : os.path.join(root, 'setup.py'),
-        'name' : app_name,
-        'id' : app_id,
-        'Id' : app_id.capitalize()
-    }
-
-    with open(os.path.join(root, 'setup.py'), 'w') as f:
-        f.write(textwrap.dedent('''\
-            VERSION = '0.0.1'
-
-            from setuptools import setup, find_packages
-
-            setup(
-                  name = '%(name)s',
-                  version = VERSION,
-                  author = '',
-                  author_email = '',
-                  description = '',
-                  license = '',
-                  keywords = '',
-                  url = '',
-                  packages = find_packages(),
-                  include_package_data = True,
-                  package_data = {'' : ['*.cfg']},
-                  zip_safe = False,
-                  install_requires = ('nagare',),
-                  message_extractors = { '%(id)s' : [('**.py', 'python', None)] },
-                  entry_points = """
-                  [nagare.applications]
-                  %(id)s = %(id)s.app:app
-                  """
-                 )
-        ''' % params))
-
-    with open(os.path.join(root, 'setup.cfg'), 'w') as f:
-        f.write(textwrap.dedent('''\
+    new_conf = ConfigObj(StringIO(textwrap.dedent('''\
             [extract_messages]
             keywords = _ , _N:1,2 , _L , _LN:1,2 , gettext , ugettext , ngettext:1,2 , ungettext:1,2 , lazy_gettext , lazy_ugettext , lazy_ngettext:1,2 , lazy_ungettext:1,2
             output_file = data/locale/messages.pot
@@ -98,20 +102,26 @@ def run(parser, options, args):
             [compile_catalog]
             directory = data/locale
             domain = messages
-        '''))
+            ''')), list_values=False)
+    new_conf.merge(old_conf)
 
-    with open(os.path.join(root, 'MANIFEST.in'), 'w') as f:
-        f.write(textwrap.dedent('''\
+    if(new_conf != old_conf):
+        if os.path.exists(filename):
+            os.rename(filename, filename+'.old')
+
+        new_conf.filename = filename
+        new_conf.write()
+
+def create_manifest_in(filename):
+    with open(filename, 'w') as f:
+        print >>f, textwrap.dedent('''\
             graft conf
             graft static
             graft data/locale
-        '''))
+        ''')
 
-    os.mkdir(os.path.join(root, app_id))
-
-    create_empty_file(os.path.join(root, app_id), '__init__.py')
-
-    with open(os.path.join(root, app_id, 'app.py'), 'w') as f:
+def create_app_py(filename, params):    
+    with open(filename, 'w') as f:
         f.write(textwrap.dedent('''\
             from __future__ import with_statement
 
@@ -186,7 +196,8 @@ def run(parser, options, args):
             app = %(Id)s
         ''' % params))
 
-    with open(os.path.join(root, app_id, 'models.py'), 'w') as f:
+def create_models_py(filename):
+    with open(filename, 'w') as f:
         f.write(textwrap.dedent('''\
             from elixir import *
             from sqlalchemy import MetaData
@@ -196,18 +207,8 @@ def run(parser, options, args):
             # Here, put the definition of your Elixir or SQLAlchemy models
         '''))
 
-    os.mkdir(os.path.join(root, 'static'))
-    create_empty_file(os.path.join(root, 'static'), '__init__.py')
-
-    os.mkdir(os.path.join(root, 'data'))
-    create_empty_file(os.path.join(root, 'data'), '__init__.py')
-
-    os.mkdir(os.path.join(root, 'data', 'locale'))
-
-    os.mkdir(os.path.join(root, 'conf'))
-    create_empty_file(os.path.join(root, 'conf'), '__init__.py')
-
-    with open(os.path.join(root, 'conf', app_id+'.cfg'), 'w') as f:
+def create_conf(filename, params):
+    with open(filename, 'w') as f:
         f.write(textwrap.dedent('''\
             [application]
             path = app %(id)s
@@ -221,6 +222,48 @@ def run(parser, options, args):
             debug = off
         ''' % params))
 
+def run(parser, options, args):
+    if len(args) == 0:
+        parser.error('an application name must be provided')
+
+    if len(args) > 1:
+        parser.error('only one application name must be provided')
+
+    root = args[0]
+    app_name = os.path.basename(root)
+    app_id = app_name.replace('-', '_')
+    
+    create_or_update(root, os.mkdir)
+
+    params = {
+        'exe' : sys.executable,
+        'root' : root,
+        'setup' : os.path.join(root, 'setup.py'),
+        'name' : app_name,
+        'id' : app_id,
+        'Id' : app_id.capitalize()
+    }
+
+    create_or_update(os.path.join(root, 'setup.py'), create_setup_py, upgrade_setup_py, params)
+    create_or_update(os.path.join(root, 'setup.cfg'), upgrade_setup_cfg, upgrade_setup_cfg)
+    create_or_update(os.path.join(root, 'MANIFEST.in'), create_manifest_in)
+
+    create_or_update(os.path.join(root, app_id), os.mkdir)
+    create_or_update(os.path.join(root, app_id, '__init__.py'), create_empty_file)
+    create_or_update(os.path.join(root, app_id, 'app.py'), create_app_py, None, params)
+    create_or_update(os.path.join(root, app_id, 'models.py'), create_models_py)
+
+    create_or_update(os.path.join(root, 'static'), os.mkdir)
+    create_or_update(os.path.join(root, 'static', '__init__.py'), create_empty_file)
+
+    create_or_update(os.path.join(root, 'data'), os.mkdir)
+    create_or_update(os.path.join(root, 'data', '__init__.py'), create_empty_file)
+    create_or_update(os.path.join(root, 'data', 'locale'), os.mkdir)
+
+    create_or_update(os.path.join(root, 'conf'), os.mkdir)
+    create_or_update(os.path.join(root, 'conf', '__init__.py'), create_empty_file)
+    create_or_update(os.path.join(root, 'conf', app_id+'.cfg'), create_conf, None, params)
+    
     print "Application '%s' created." % app_name
     print
     print textwrap.dedent("""\
