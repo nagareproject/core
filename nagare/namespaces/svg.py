e7744cedf3a9dee1391382305f185d9c4bd4ea6d
Vincent Rialland <vincent.rialland@gmail.com>
2016-10-13 11:36:57 +0200

    Init SVG renderer and tests

---
+++ nagare/namespaces/svg.py
@@ -0,0 +1,17 @@
+#--
+# Copyright (c) 2008-2016 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+#--
+
+"""The SVG renderer
+"""
+
+from nagare.namespaces.xml import XmlRenderer, TagProp
+
+
+class SVGRenderer(XmlRenderer):
+    pass
---
+++ nagare/test/test_svg.py
@@ -0,0 +1,34 @@
+# Encoding: utf-8
+
+#--
+# Copyright (c) 2008-2016 Net-ng.
+# All rights reserved.
+#
+# This software is licensed under the BSD License, as described in
+# the file LICENSE.txt, which you should have received as part of
+# this distribution.
+#--
+
+import unittest
+
+from nagare.namespaces.svg import SVGRenderer
+
+
+class SVG(unittest.TestCase):
+    def test_attributes(self):
+        all_attribs = ('a', 'altGlyph', 'altGlyphDef', 'altGlyphItem', 'animate', 'animateColor', 'animateMotion',
+                       'animateTransform', 'circle', 'clipPath', 'color-profile', 'cursor', 'defs', 'desc', 'ellipse',
+                       'feBlend', 'feColorMatrix', 'feComponentTransfer', 'feComposite', 'feConvolveMatrix',
+                       'feDiffuseLighting', 'feDisplacementMap', 'feDistantLight', 'feFlood', 'feFuncA', 'feFuncB',
+                       'feFuncG', 'feFuncR', 'feGaussianBlur', 'feImage', 'feMerge', 'feMergeNode', 'feMorphology',
+                       'feOffset', 'fePointLight', 'feSpecularLighting', 'feSpotLight', 'feTile', 'feTurbulence',
+                       'filter', 'font', 'font-face', 'font-face-format', 'font-face-name', 'font-face-src',
+                       'font-face-uri', 'foreignObject', 'g', 'glyph', 'glyphRef', 'hkern', 'image', 'line',
+                       'linearGradient', 'marker', 'mask', 'metadata', 'missing-glyph', 'mpath', 'path', 'pattern',
+                       'polygon', 'polyline', 'radialGradient', 'rect', 'script', 'set', 'stop', 'style', 'svg',
+                       'switch', 'symbol', 'text', 'textPath', 'view', 'title', 'tref', 'vkern', 'tspan', 'use')
+        for attr in all_attribs:
+            attr_name = attr.replace('-', '_')
+            attrib = getattr(SVGRenderer, attr_name)
+            self.assertIsNotNone(attrib, '{} not fount in SVGRenderer'.format(attr))
+            self.assertEqual(attrib._name, attr)
