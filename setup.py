dee9c0425e18b1df6e06de646795a23c338f8928
Alain Poirier <alain.poirier@net-ng.com>
2018-01-25 12:29:14 +0100

    Version 0.5.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import textwrap
 
 from setuptools import setup, find_packages
 
-VERSION = '0.5.0'
+VERSION = '0.5.1'
 
 
 # -----------------------------------------------------------------------------
