aad5a51c0019a8e9966f401db5d87dd1f850a13a
Alain Poirier <alain.poirier@net-ng.com>
2020-04-16 18:17:49 +0200

    Client-side callbacks parameters

--- nagare/services/callbacks.py
+++ nagare/services/callbacks.py
@@ -11,15 +11,25 @@
 Manage the dictionary of the ids / callbacks associations
 """
 
+import os
 import re
+import json
+import base64
 from collections import defaultdict
+
+from webob import exc
 
 from nagare import partial
 from nagare.services import plugin
 from nagare.continuation import Continuation
 
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
 ACTION_PREFIX = '_action'
 ACTION_SYNTAX = re.compile(ACTION_PREFIX + r'(\d)(\d+)((.x)|(.y))?(#(.*))?$')
+
+callbacks_service = None
 
 
 class CallbackLookupError(LookupError):
@@ -29,8 +39,47 @@ class CallbackLookupError(LookupError):
 class CallbacksService(plugin.Plugin):
     LOAD_PRIORITY = 110
 
-    @classmethod
-    def handle_request(cls, chain, callbacks, request, response, root, **params):
+    def __init__(self, name, dist, **config):
+        global callbacks_service
+        super(CallbacksService, self).__init__(name, dist, **config)
+
+        key = os.urandom(16)
+        self.cipher = Cipher(algorithms.AES(key), modes.ECB(), default_backend())
+        callbacks_service = self
+
+    @staticmethod
+    def pad(buf, l, padding):
+        nb = len(buf) % l
+        return buf + padding * (nb and (l - nb))
+
+    def encode_client_params(self, client_params):
+        if not client_params:
+            return ''
+
+        encryptor = self.cipher.encryptor()
+
+        v = json.dumps(client_params).encode('utf-8')
+        v = self.pad(v + b'#', 16, b' ')
+        v = encryptor.update(v) + encryptor.finalize()
+        v = base64.urlsafe_b64encode(v).decode('ascii')
+
+        return v
+
+    def decode_client_params(self, client_params):
+        if not client_params:
+            return {}
+
+        decryptor = self.cipher.decryptor()
+
+        v = base64.urlsafe_b64decode(client_params)
+        v = decryptor.update(v) + decryptor.finalize()
+        v = v.rstrip(b' ')
+        if not v.endswith(b'#'):
+            raise exc.HTTPNotFound()
+
+        return json.loads(v[:-1])
+
+    def handle_request(self, chain, callbacks, request, response, root, **params):
         """Call the actions associated to the callback identifiers received
 
         In:
@@ -57,16 +106,17 @@ class CallbacksService(plugin.Plugin):
 
         render = None
 
-        for (callback_type, callback_id, param, client_params), values in sorted(actions.items()):
-            request.client_params['_p'] = client_params or {}
-
+        for (callback_type, callback_id, complement, client_params), values in sorted(actions.items()):
             try:
-                _, f, with_request, render, args, kw = callbacks[int(callback_id)]
+                _, f, with_request, render, callback_args, kw = callbacks[int(callback_id)]
             except KeyError:
                 raise CallbackLookupError(callback_id)
 
             if f is None:
                 continue
+
+            callback_params = self.decode_client_params(client_params or request.params.get('_p'))
+            callback_params.update(kw)
 
             # ``callback_type``:
             #
@@ -83,18 +133,18 @@ class CallbacksService(plugin.Plugin):
                 f = partial.Partial(f, request, response)
 
             if callback_type == '3':
-                f(*(args + (tuple(values),)), **kw)
+                f(*(callback_args + (tuple(values),)), **callback_params)
             else:
                 for value in values:
                     if callback_type in ('0', '2'):
-                        f(*args, **kw)
+                        f(*callback_args, **callback_params)
                     elif callback_type == '1':
-                        f(*(args + (value,)), **kw)
+                        f(*(callback_args + (value,)), **callback_params)
                     elif callback_type in ('4', '5', '6'):
-                        Continuation(f, *args, **kw)
-                    elif (callback_type == '7') and param:
-                        args += (param == '.y', int(values[0]))
-                        Continuation(f, *args, **kw)
+                        Continuation(f, *callback_args, **callback_params)
+                    elif (callback_type == '7') and complement:
+                        callback_args += (complement == '.y', int(values[0]))
+                        Continuation(f, *callback_args, **callback_params)
 
         return chain.next(
             callbacks=callbacks,
